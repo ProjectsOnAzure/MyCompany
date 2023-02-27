a8a33838755dfaa6c2d92d289eef4f402f922c0f
Саша <@DESKTOP-3MSPUED>
2023-02-27 02:28:58 +0200

    Добавьте файлы проекта.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DepartmentAzureGitHub.Models;
+
+namespace DepartmentAzureGitHub.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace DepartmentAzureGitHub.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepName { get; set; }
+        public virtual List<Employee> Employees { get; set; }
+    }
+}
